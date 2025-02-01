9e64e5ff8bb44438e71f7dade3b88fe92795db56
nourchamas <chamasnour87@gmail.com>
2025-02-01 22:24:08 +0200

    Paramètres Mongoose ajoutés.

--- argent_Bank-projet11/ArgentBank-Backend-main/ArgentBank-Backend-main/server/database/connection.js
+++ argent_Bank-projet11/ArgentBank-Backend-main/ArgentBank-Backend-main/server/database/connection.js
@@ -1,13 +1,17 @@
-const mongoose = require('mongoose')
-const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+const mongoose = require("mongoose");
+const databaseUrl = process.env.DATABASE_URL || "mongodb://localhost/argentBankDB";
 
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
-    console.log('Database successfully connected')
+    await mongoose.connect(databaseUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      ssl: true,
+      sslValidate: true,
+    });
+    console.log("Database successfully connected");
   } catch (error) {
-    console.error(`Database Connectivity Error: ${error}`)
-    throw new Error(error)
+    console.error(`Database Connectivity Error: ${error}`);
+    throw new Error(error);
   }
-}
+};
