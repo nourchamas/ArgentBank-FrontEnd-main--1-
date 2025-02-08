0d25ed4e38e534b115aea29c8aaec060e9ced516
nourchamas <chamasnour87@gmail.com>
2025-02-08 11:58:18 +0100

    import Error

--- argent_Bank-projet11/frontend/src/App.jsx
+++ argent_Bank-projet11/frontend/src/App.jsx
@@ -1,9 +1,10 @@
-
+// Importation des styles CSS et des composants nécessaires
 import "./App.css";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home/Home";
+import Error from "./pages/Error/Error";
 import Login from "./pages/Login/Login";
 import Profile from "./pages/Profile/Profile";
 import { useDispatch, useSelector } from "react-redux";
@@ -12,11 +13,16 @@ import { useEffect } from "react";
 import { loginSuccess } from "./redux/actions/auth.actions";
 
 function App() {
+   // Sélection de l'état de connexion 
+   // de l'utilisateur à partir du store Redux
   const isConnected = useSelector((state) => state.auth.isConnected);
+  // Déclaration du dispatch pour envoyer des actions Redux
   const dispatch = useDispatch();
-
-  useEffect(() => {
+  // Utilisation de useEffect pour vérifier si un token existe 
+   useEffect(() => {
+    // Récupération du token depuis localStorage ou sessionStorage
     const token = localStorage.getItem("token") || sessionStorage.getItem("token");
+     // Si un token est trouvé, dispatch de l'action loginSuccess avec le token
     if (token) {
       dispatch(loginSuccess(token));
     }
